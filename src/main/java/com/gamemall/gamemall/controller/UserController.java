acc277ceaf3057c88b30f88beb35c2c936a18820
lijunhong <lijunhong@hikcreate.com>
2020-03-23 08:38:04 +0800

    sss

--- src/main/java/com/gamemall/gamemall/controller/UserController.java
+++ src/main/java/com/gamemall/gamemall/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.gamemall.gamemall.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gamemall.gamemall.entity.User;
 import com.gamemall.gamemall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/user")
@@ -17,11 +22,39 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "login")
-    public User login(@RequestBody JsonNode jsonNode) throws Exception {
+    @RequestMapping(method = RequestMethod.POST, path = "/login")
+    public ResponseEntity login(@RequestBody JsonNode jsonNode) throws Exception {
         //用户认证
         String email = jsonNode.path("email").textValue();
         String password = jsonNode.path("password").textValue();
-        return userService.getAccount(email,password);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.getAccount(email,password));
     }
+
+    @GetMapping("/hello")
+    ResponseEntity hello() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Your age is " );
+    }
+
+//    @Autowired
+//    private UserService userService;
+//
+//    @RequestMapping(method = RequestMethod.POST, path = "/register")
+//    @ResponseBody
+//    public ResponseEntity addUser(@RequestBody JsonNode jsonNode,
+//                               @RequestParam(required = false) String select) {
+////        EnsureLabUtil.ensureSMPermission();
+//        User user;
+//        try {
+//            user = new ObjectMapper().readValue(jsonNode.traverse(), User.class);
+//        } catch (IOException e) {
+////            throw new ArgumentErrorException("请查看请求体");
+//        }
+//        userService.addUser(user);
+//        if (select != null) {
+//            return ResponseEntity.status(HttpStatus.CREATED).body(new ObjectSelector().mapObject(userRole, select));
+//        }
+//        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+//    }
 }
